fe395f54ad0e72c2fb80155c247f849e3864ff83
Enric Piferrer Torres <enric.piferrer@travelperk.com>
2021-07-23 11:55:31 -0700

    Manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -14,6 +14,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validate_data)
+    
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
